0911d5590f1c6b54f3b4e535b8cc9bc252eb2f9d
yuliiaivanechko <yuliaivanechko@gmail.com>
2022-02-02 07:44:41 +0200

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Yuliia Ivanechko on 02.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
